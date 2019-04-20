20f55c96e1b1d3ab5bad81038a85a809f0484a77
cvssea <42877793+cvssea@users.noreply.github.com>
2019-04-20 23:36:20 +0300

    implement all animations

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
 const focus = document.querySelector(".focus");
-const buttons = document.querySelectorAll("button");
+const toggleIn = document.querySelector(".signin-toggle");
+const toggleUp = document.querySelector(".signup-toggle");
+const buttons = document.querySelectorAll(".btn-focus");
+const forms = document.querySelectorAll(".form-container");
 
+// Main event listener - applies to both toggle buttons
 buttons.forEach(button => {
-  button.addEventListener("click", slide.bind(focus));
+  button.addEventListener("click", action);
 });
 
-function slide() {
-  if (this.classList.contains("slide-r")) {
-    this.classList.remove("slide-r");
-    this.classList.add("slide-l");
+function action() {
+  focusClasses();
+  formClasses();
+}
+
+// Handle sliding focus area classes and togglers content
+function focusClasses() {
+  if (focus.classList.contains("slide-r")) {
+    focus.classList.remove("slide-r");
+    focus.classList.add("slide-l");
+    toggleUp.classList.add("shift");
+    toggleIn.classList.remove("shift");
   } else {
-    this.classList.add("slide-r");
-    this.classList.remove("slide-l");
+    focus.classList.add("slide-r");
+    focus.classList.remove("slide-l");
+    toggleIn.classList.add("shift");
+    toggleUp.classList.remove("shift");
   }
+}
+
+// Handle show/hide forms classes
+function formClasses() {
+  forms.forEach(form => {
+    // Fix disappearing .signin-form when switching hide/show classes
+    if (form.classList.contains('signin-form')) {
+      form.style.visibility = 'initial';
+    }
+
+    if (form.classList.contains("hide")) {
+      form.classList.remove("hide");
+      form.classList.add("show");
+    } else {
+      form.classList.remove("show");
+      form.classList.add("hide");
+    }
+  });
 }
